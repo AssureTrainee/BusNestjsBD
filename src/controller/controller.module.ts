3774125f1ddb600855048bbdef052a68a894df18
Juan Pablo Nuñez <juanpanm2409@gmail.com>
2023-05-29 20:30:15 -0400

    Fixing app.module

--- src/controller/controller.module.ts
+++ src/controller/controller.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { RouteController } from './route/route.controller';
 import { ServiceModule } from 'src/service/service.module';
+import { PassengerController } from './passenger/passenger.controller';
+import { PersonController } from './person/person.controller';
 
 @Module({
   imports: [ServiceModule],
-  controllers: [RouteController],
+  controllers: [RouteController, PassengerController, PersonController],
 })
 export class ControllerModule {}
